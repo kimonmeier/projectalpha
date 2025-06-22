1c468e46e91ff4fb58361fbeec0cc66bee88301a
Kimon Meier <kimon.meier@gmail.com>
2025-06-22 18:39:27 +0200

    Added CSS Build

---
+++ ProjectAlpha/ProjectAlpha/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./**/*.{razor,html,cshtml}",
+		"../ProjectAlpha.Client/**/*.{razor,html,cshtml}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
