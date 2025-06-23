65591373dcdd11253372e661b456b74bac31c22c
Kimon Meier <kimon.meier@gmail.com>
2025-06-23 15:01:51 +0200

    Added first version for the rendering

---
+++ ProjectAlpha/ProjectAlpha.Client/BenchmarkHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace ProjectAlpha.Client;
+
+public class BenchmarkHelper
+{
+    private readonly Dictionary<string, Stopwatch> _benchmarks = new();
+    private readonly Dictionary<string, int> _executionTimes = new();
+    private readonly Dictionary<string, List<int>> _executionRuns = new();
+    
+    public void Start(string name)
+    {
+        _benchmarks[name] = Stopwatch.StartNew();
+
+        _executionRuns.TryAdd(name, new List<int>());
+        _executionTimes.TryAdd(name, 0);
+        _executionTimes[name] += 1;
+    }
+    
+    public void Stop(string name)
+    {
+        var stopwatch = _benchmarks[name];
+        stopwatch.Stop();
+        _executionRuns[name].Add((int) stopwatch.Elapsed.TotalMilliseconds);
+        
+        _benchmarks.Remove(name);
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Benchmark Results:");
+        Console.WriteLine("==================");
+        Console.WriteLine("Name: Times: Average: Total");
+        foreach (var (name, times) in _executionTimes)
+        {
+            var average = _executionRuns[name].Average();
+            Console.WriteLine($"{name}: {times}: {TimeSpan.FromMilliseconds(average):g} {TimeSpan.FromMilliseconds(average * times):g}");
+        }
+        Console.WriteLine("==================");
+    }
+
+    public void Clear()
+    {
+        _benchmarks.Clear();
+        _executionTimes.Clear();
+        _executionRuns.Clear();
+    }
+    
+    
+    
+}
